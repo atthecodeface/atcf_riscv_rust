f45ab45bd785837d4af85ac904ac43c7233d5cba
Gavin Stark <atthecodeface@gmail.com>
2019-10-16 20:38:35 +0100

    [microos] Fixed UART address

--- microos/src/main.rs
+++ microos/src/main.rs
@@ -14,9 +14,9 @@ pub const APB_TIMER:            *mut u32 = apb_dev!(0,0) as *mut u32;
 pub const APB_GPIO:             *mut u32 = apb_dev!(1,0) as *mut u32;
 pub const APB_DPRINTF:          *mut u32 = apb_dev!(2,0) as *mut u32;
 pub const APB_CSR:              *mut u32 = apb_dev!(3,0) as *mut u32;
-pub const APB_UART:             *mut u32 = apb_dev!(4,0) as *mut u32;
-pub const APB_RV_SRAM:          *mut u32 = apb_dev!(7,0) as *mut u32;
-pub const APB_FB_SRAM:          *mut u32 = apb_dev!(9,0) as *mut u32;
+pub const APB_RV_SRAM:          *mut u32 = apb_dev!(4,0) as *mut u32;
+pub const APB_FB_SRAM:          *mut u32 = apb_dev!(7,0) as *mut u32;
+pub const APB_UART:             *mut u32 = apb_dev!(9,0) as *mut u32;
 pub const APB_DPRINTF_UART:     *mut u32 = apb_dev!(10,0) as *mut u32;
 pub const APB_RISCV_DBG:        *mut u32 = apb_dev!(11,0) as *mut u32;
 

cbe2ed1939649def3d3f2c036afc803623372a4f
Gavin J Stark <atthecodeface@gmail.com>
2019-10-17 11:17:16 +0100

    [make] Updated to build on other platforms

--- riscv_base/build.rs
+++ riscv_base/build.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs::File;
-use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 use std::path::Path;
@@ -10,26 +9,28 @@ fn main() {
     // Put the linker script somewhere the linker can find it
     let out_dir = env::var("OUT_DIR").unwrap();
     let out     = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let target  = env::var("TARGET").unwrap();
     let name    = "riscv_base";
+    let gnu_tools = env::var("GNU_TOOLS").unwrap(); // riscv-elf-
+    let gcc       = &format!("{}gcc", gnu_tools);
+    let ar        = &format!("{}ar",  gnu_tools);
 
     println!("cargo:rerun-if-changed=src/init.S");
     println!("cargo:rerun-if-changed=src/utils.S");
-    Command::new("riscv32-elf-gcc").args(&["-c",
+    Command::new(gcc).args(&["-c",
                                            "-mabi=ilp32",
                                            "-march=rv32imac",
                                            "src/init.S",
                                            "-o"])
         .arg(&format!("{}/init.o", out_dir))
         .status().unwrap();
-    Command::new("riscv32-elf-gcc").args(&["-c",
+    Command::new(gcc).args(&["-c",
                                            "-mabi=ilp32",
                                            "-march=rv32imac",
                                            "src/utils.S",
                                            "-o"])
         .arg(&format!("{}/utils.o", out_dir))
         .status().unwrap();
-    Command::new("riscv32-elf-ar").args(&["crs"])
+    Command::new(ar).args(&["crs"])
         .arg(&format!("lib{}.a", name))
         .args(&["init.o"])
         .args(&["utils.o"])

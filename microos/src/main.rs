dc4a9ee8d347d51c3c6f139206abfbb2d0474de3
Gavin Stark <atthecodeface@gmail.com>
2019-10-23 10:32:44 +0100

    [xilinx] Tidied up

--- microos/src/main.rs
+++ microos/src/main.rs
@@ -4,6 +4,8 @@
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 extern crate riscv_base;
 
+//const WAIT_SLEEP:u32 =1000;
+const WAIT_SLEEP:u32 =10;
 
 fn uart_loopback() -> () {
     riscv_base::uart::config();
@@ -25,7 +27,7 @@ fn uart_loopback() -> () {
 
 fn wait(v:u32) -> () {
    for _ in 0..v {
-       unsafe {riscv_base::sleep(1000)};
+       unsafe {riscv_base::sleep(WAIT_SLEEP)};
    }
 }
 
@@ -34,60 +36,49 @@ fn show_gpio_in() -> () {
    riscv_base::dprintf::write1(0,0x8100ffff|((riscv_base::gpio::get_inputs()&0xff)<<16));
 }
 
+fn vcu108_i2c_drive_and_wait(gpio:u32, delay:u32) -> () {
+    riscv_base::gpio::set_outputs(gpio);
+    wait(delay);
+}
+
 fn vcu108_i2c_start(gpio_base:u32) -> () {
-    riscv_base::gpio::set_outputs(gpio_base | 0);   //  SDA high, SCL high
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0x8); //  Start (SDA low, SCL stays high)
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0xa); //  SDA low SCL low
-    wait(2);
+    vcu108_i2c_drive_and_wait(gpio_base | 0x0, 2);   //  SDA high, SCL high
+    vcu108_i2c_drive_and_wait(gpio_base | 0x8, 2);   //  SDA low, SCL high
+    vcu108_i2c_drive_and_wait(gpio_base | 0xa, 2);   //  SDA low, SCL low
 }
 
 fn vcu108_i2c_stop(gpio_base:u32) -> () {
-    riscv_base::gpio::set_outputs(gpio_base | 0xa); //  SDA low SCL low
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0x8); //  SCL high, SDA low
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0);   //  SDA high, SCL high
-    wait(2);
+    vcu108_i2c_drive_and_wait(gpio_base | 0xa, 2);   //  SDA low, SCL low
+    vcu108_i2c_drive_and_wait(gpio_base | 0x8, 2);   //  SDA low, SCL high
+    vcu108_i2c_drive_and_wait(gpio_base | 0x0, 2);   //  SDA high, SCL high
 }
 
 fn vcu108_i2c_cont(gpio_base:u32) -> () {
-    riscv_base::gpio::set_outputs(gpio_base | 0xa); //  SDA low SCL low
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0x2); //  SDA high, SCL low
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0);   //  SDA high, SCL high
-    wait(2);
+    vcu108_i2c_drive_and_wait(gpio_base | 0xa, 2);   //  SDA low, SCL low
+    vcu108_i2c_drive_and_wait(gpio_base | 0x2, 2);   //  SDA high, SCL low
+    vcu108_i2c_drive_and_wait(gpio_base | 0x0, 2);   //  SDA high, SCL high
 }
 
-fn vcu108_i2c_output_bit(gpio_base:u32, data:bool) -> () {
-    riscv_base::gpio::set_outputs(gpio_base | (0x2 | (if data {0} else {8}))); //  Keep SCL low, SDA to bit value
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | (0x0 | (if data {0} else {8}))); //  Let SCL float high, SDA hold
-    wait(2);
- show_gpio_in();
-    riscv_base::gpio::set_outputs(gpio_base | (0x2 | (if data {0} else {8}))); //  Pull SCL low, SDA hold
-    wait(2);
-}
-
-fn vcu108_i2c_read_bit(gpio_base:u32) -> bool {
-    riscv_base::gpio::set_outputs(gpio_base | 0x2 ); //  Keep SCL low, SDA float
-    wait(2);
-    riscv_base::gpio::set_outputs(gpio_base | 0x0 ); //  Rising edge clock (hope SDA is low for ack)
-    wait(1);
-    let r=(riscv_base::gpio::get_inputs()&1)==1;
-    wait(1);
-    riscv_base::gpio::set_outputs(gpio_base | 0x2 ); //  Pull SCL low, SDA hold
-    wait(2);
-    r
+fn vcu108_i2c_output_bit(gpio_base:u32, data:bool) -> bool {
+    if data {
+        vcu108_i2c_drive_and_wait(gpio_base | 0x2, 2);   //  SDA high, SCL low
+        vcu108_i2c_drive_and_wait(gpio_base | 0x0, 2);   //  SDA high, SCL high
+        let r=(riscv_base::gpio::get_inputs()&1)==1;
+        vcu108_i2c_drive_and_wait(gpio_base | 0x2, 2);   //  SDA high, SCL low
+        r
+    } else {
+        vcu108_i2c_drive_and_wait(gpio_base | 0xa, 2);   //  SDA low, SCL low
+        vcu108_i2c_drive_and_wait(gpio_base | 0x8, 2);   //  SDA low, SCL high
+        vcu108_i2c_drive_and_wait(gpio_base | 0xa, 2);   //  SDA low, SCL low
+        false
+    }
 }
 
 fn vcu108_i2c_output_byte(gpio_base:u32, data:u32) -> bool {
    for i in 0..8 {
      vcu108_i2c_output_bit(gpio_base, ((data>>(7-i))&1)==1 );
    }
-   let nack = vcu108_i2c_read_bit(gpio_base);
+   let nack = vcu108_i2c_output_bit(gpio_base, true); // output a 1 (others may pull it low)
    riscv_base::dprintf::wait();
    riscv_base::dprintf::write4(0,(0x41636b87, (if nack {0} else {1}), 0xffffffff,0));
    false
@@ -99,8 +90,6 @@ fn vcu108_i2c_exec(gpio_base:u32, num_out:u32, num_in:u32, cont:bool, data_in:u3
     vcu108_i2c_start(gpio_base);
     if num_out>0 {
         for _ in 0..num_out {
-   riscv_base::dprintf::wait();
-           riscv_base::dprintf::write4(0,(0x44617487, data, 0xffffffff,0));
             if okay { okay = vcu108_i2c_output_byte(gpio_base, data);
                       data = data >> 8;
             }
@@ -160,6 +149,6 @@ pub extern "C" fn main() -> () {
     vcu108_i2c_exec(gpio_base, 1, 1, false, (0x75<<1)|1 );
     vcu108_i2c_reset(gpio_base);
     riscv_base::dprintf::wait();
-    riscv_base::dprintf::write1(0,(0x454e44ff));
+    riscv_base::dprintf::write1(0,0x454e44ff);
     loop {};
 }

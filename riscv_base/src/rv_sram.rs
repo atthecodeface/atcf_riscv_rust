90cd25733f83907af52887eb5512635475cd719f
Gavin Stark <atthecodeface@gmail.com>
2019-11-12 15:28:24 +0000

    [vcu108] Added in rv_sram

---
+++ riscv_base/src/rv_sram.rs
@@ -0,0 +1,20 @@
+
+// address = 0
+// data = 1
+// control = 2
+// data_inc = 4...7
+// windowed=128..255
+
+pub fn set_control(data:u32) {
+    unsafe {
+        let apb_sram_ctrl : *mut u32 = super::minimal::APB_RV_SRAM.offset(2);
+        core::ptr::write_volatile(apb_sram_ctrl,data)
+    };
+}
+
+pub fn get_control() -> u32 {
+    return unsafe {
+        let apb_sram_ctrl : *const u32 = super::minimal::APB_RV_SRAM.offset(2);
+        core::ptr::read_volatile(apb_sram_ctrl)
+    };
+}

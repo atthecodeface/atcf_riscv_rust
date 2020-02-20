c42b6e0c404ac2867d516b805a94a0c6b32c6abd
Gavin J Stark <atthecodeface@gmail.com>
2020-02-20 17:54:09 +0000

    [pxeboot] Now operational

--- pxeboot_rom/src/loader.rs
+++ pxeboot_rom/src/loader.rs
@@ -12,17 +12,6 @@ impl SubLoader {
 impl pxeboot::loader::SubLoader for SubLoader {
     fn copy_memory(&self, source:&[u8], dest:u32) -> bool {
       riscv_base::dprintf::write4(0,(0x4d454d87,dest,0xffffffff,0));
-    /*
-            unsafe {
-                let mut data_ptr: *const u32  = source.as_ptr() as *const u32;
-                for _ in 0..16 {
-                    let d = *data_ptr;
-                    riscv_base::dprintf::write4(0,(0x87,d,0xffffffff,0));
-                    data_ptr = data_ptr.offset(1);
-                }
-            }
-        //println!("Copy memory to {:?}", dest);
-  */
         unsafe {
             let mut dest_ptr: *mut u8 = dest as *mut u8;
             for i in 0..source.len() {
@@ -34,6 +23,16 @@ impl pxeboot::loader::SubLoader for SubLoader {
     }
     fn execute(&self, address:u32) {
         riscv_base::dprintf::write4(0,(0x45584587,address,0xffffffff,0));
+            unsafe {
+                let mut data_ptr: *const u32  = address as *const u32;
+                for _ in 0..64 {
+                    let d = *data_ptr;
+                    riscv_base::dprintf::write4(0,(0x87,d,0xffffffff,0));
+                    data_ptr = data_ptr.offset(1);
+                }
+            }
+        //println!("Copy memory to {:?}", dest);
+unsafe { riscv_base::sleep(1<<20);}
         unsafe {riscv_base::exec_at(address);}
         //println!("Execute at {:?}", address);
         loop {}
